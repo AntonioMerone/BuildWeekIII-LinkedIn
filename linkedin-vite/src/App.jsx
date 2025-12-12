64f2c1584949f9b9ff4c323387d8a817d370ac47
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2025-12-12 09:41:19 +0100

    adds Jobs page

--- linkedin-vite/src/App.jsx
+++ linkedin-vite/src/App.jsx
@@ -1,16 +1,17 @@
-import "./App.css";
-import { Provider } from "react-redux";
-import { Container, Row, Col } from "react-bootstrap";
-import Profile from "./components/Profile";
-import MyNavbar from "./components/MyNavbar";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import ProfileSidebar from "./components/ProfileSidebar";
-import ProfileFooter from "./components/ProfileFooter";
-import { setProfileData } from "./reducers/profileReducerData.js";
-import { store } from "./store.js";
-import Home from "./components/home/Home.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css"
+import { Provider } from "react-redux"
+import { Container, Row, Col } from "react-bootstrap"
+import Profile from "./components/Profile"
+import MyNavbar from "./components/MyNavbar"
+import { useDispatch } from "react-redux"
+import { useEffect } from "react"
+import ProfileSidebar from "./components/ProfileSidebar"
+import ProfileFooter from "./components/ProfileFooter"
+import { setProfileData } from "./reducers/profileReducerData.js"
+import { store } from "./store.js"
+import Home from "./components/home/Home.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Jobs from "./components/jobs/Jobs.jsx"
 
 function App() {
   return (
@@ -19,49 +20,50 @@ function App() {
         <Linkedin />
       </Provider>
     </>
-  );
+  )
 }
 
 function Linkedin() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   //const params = useParams();
-  const baseEndpoint = "https://striveschool-api.herokuapp.com/api/profile/me";
+  const baseEndpoint = "https://striveschool-api.herokuapp.com/api/profile/me"
   const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTM5NGU2OTYwMWIzODAwMTU0Nzk1YTIiLCJpYXQiOjE3NjUzNjMzMTQsImV4cCI6MTc2NjU3MjkxNH0.xE3rxZzOErGAexkCYzlCl4YP7kKO8OrhXQ5h8SqIY-w";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTM5NGU2OTYwMWIzODAwMTU0Nzk1YTIiLCJpYXQiOjE3NjUzNjMzMTQsImV4cCI6MTc2NjU3MjkxNH0.xE3rxZzOErGAexkCYzlCl4YP7kKO8OrhXQ5h8SqIY-w"
 
   useEffect(() => {
-    getProfileData();
+    getProfileData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [])
   const getProfileData = async () => {
     try {
       const response = await fetch(baseEndpoint, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-      });
+      })
 
       if (response.ok) {
-        const data = await response.json(); // don't destructure unless API wraps it
-        dispatch(setProfileData(data));
+        const data = await response.json() // don't destructure unless API wraps it
+        dispatch(setProfileData(data))
       } else {
-        alert("Error fetching results");
+        alert("Error fetching results")
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
   return (
     <>
       <Router>
         <Routes>
           <Route path="/profile" element={<LinkedinProfile />} />
           <Route path="/" element={<Home />} />
+          <Route path="/jobs" element={<Jobs />} />
         </Routes>
       </Router>
     </>
-  );
+  )
 }
 
 function LinkedinProfile({ user }) {
@@ -80,7 +82,7 @@ function LinkedinProfile({ user }) {
       </Container>
       <ProfileFooter />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ linkedin-vite/src/components/jobs/Jobs.jsx
@@ -0,0 +1,5 @@
+function Jobs() {
+  return <h1>Pagina Jobs</h1>
+}
+
+export default Jobs
