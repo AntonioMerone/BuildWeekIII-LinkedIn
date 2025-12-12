a3c0a44dedaf3d630a868691b84c4dc4c324e03f
virgilistrate <virgilistrate24@gmail.com>
2025-12-12 11:47:51 +0100

    adds jobs

--- linkedin-vite/src/store.js
+++ linkedin-vite/src/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from "redux";
 import { profileDataReducer } from "./reducers/profileReducerData";
 import { postsDataReducer } from "./reducers/postsReducerData";
+import { jobsReducer } from "./reducers/jobsReducer";
 
 const rootReducer = combineReducers({
   profileData: profileDataReducer,
   postsData: postsDataReducer,
+  jobs: jobsReducer,
 });
 
 export const store = createStore(rootReducer);
